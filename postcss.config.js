e20f9dd4de9f24ba6628a248eb2ff69f745f67dd
damoncheng <354092565@qq.com>
2025-07-04 20:33:14 +0800

    adjust postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-// postcss.config.js
 module.exports = {
-  plugins: [
-    require('tailwindcss'), // 引入 Tailwind CSS
-    require('autoprefixer'), // 自动添加浏览器前缀
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
